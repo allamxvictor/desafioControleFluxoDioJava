e94045cab0a52e4b727c325f5ff03b368b367fdf
Allan Victor <allamxvictorx@gmail.com>
2024-08-13 05:16:05 -0300

    Desafio concluído.

---
+++ src/dio/desafioControleFluxo/ParametrosInvalidosException.java
@@ -0,0 +1,8 @@
+package dio.desafioControleFluxo;
+
+public class ParametrosInvalidosException extends Exception {
+	 
+	public ParametrosInvalidosException(String msg) {
+	        super(msg);
+	}
+}
